c51d12e9544123b254f4dbfdca2622142c062dd7
Juliano Elias <julianorelias94@hotmail.com>
2024-04-27 01:06:28 -0300

    Vetor tipo classe, inserindo produtos

---
+++ VetorOO/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using VetorOO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos produtos deseja cadastrar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for(int i=0; i < n; i++)
+            {
+                Console.WriteLine("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o preço do produto: ");
+                double preco = double.Parse(Console.ReadLine());
+
+                vect[i] = new Produto(nome, preco);
+                
+            }
+
+            double soma = 0;
+
+            for(int i= 0; i<n;i++)
+            {
+                soma += vect[i].Preco;
+                //Console.WriteLine(vect[i].Nome+" "+ vect[i].Preco);
+
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("A média é "+media.ToString("F2"));
+
+
+        }
+    }
+}
